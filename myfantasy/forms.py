2510b7cd835fb363d582c11d26655bf00a47a592
zitu46 <arif.arman046@gmail.com>
2015-02-24 18:54:02 +0600

    player and team changed : PC

--- myfantasy/forms.py
+++ myfantasy/forms.py
@@ -1,2 +1,8 @@
 from django import forms
 
+class TestForm(forms.Form):
+    test_name = forms.CharField(label = 'your name', max_length=100)
+
+class TeamForm(forms.Form):
+    pid = forms.IntegerField()
+
--- myfantasy/views.py
+++ myfantasy/views.py
@@ -1,14 +1,13 @@
 from django.shortcuts import render, get_object_or_404, get_list_or_404
 from myfantasy.models import Player, Team
 from django import forms
+from myfantasy.forms import *
 import json
 from django.http.response import HttpResponse
 # Create your views here.
 
 text = []
-
-class TestForm(forms.Form):
-    test_name = forms.CharField(label = 'your name', max_length=100)
+myteam = []
 
 def test(request):
 
@@ -22,8 +21,15 @@ def test(request):
     return render(request, 'myfantasy/test.html', {'form': form, 'text': text})
 
 def index(request):
+    if request.method == 'POST' and request.is_ajax():
+        form = TeamForm(request.POST)
+        if form.is_valid():
+            if myteam.count(form.cleaned_data['pid']) == 0:
+                myteam.append(form.cleaned_data['pid'])
+    else:
+        form = TeamForm()
     players = Player.objects.all()
-    return render(request, 'myfantasy/index.html', {'players' : players})
+    return render(request, 'myfantasy/index.html', {'players' : players, 'myteam': myteam})
 
 def player_details(request, player_id):
     player = get_object_or_404(Player, pk = player_id)
@@ -31,4 +37,6 @@ def player_details(request, player_id):
 
 def team_details(request, team_id):
     team = get_object_or_404(Team, pk = team_id)
-    return render(request, 'myfantasy/team.html', {'team' : team})
+    players = Player.objects.all().filter(team = team_id).order_by('position')
+    #return render(request, 'myfantasy/test2.html', {'team': team, 'players': players})
+    return render(request, 'myfantasy/team.html', {'team' : team, 'players': players})
